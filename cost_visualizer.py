558a9f7ca4b847d62a9007eb84d1dcd0616bc736
reoyamanaka <ryamanaka807@gmail.com>
2021-01-15 17:06:03 -0500

    inserted sample data and completed cost categorization in dictionary

--- cost_visualizer.py
+++ cost_visualizer.py
@@ -2,3 +2,23 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+
+df = pd.read_csv('cost_sheet.csv', header = 0, index_col = 0)
+
+costDict = {}
+
+for costType in df["Type"].unique():
+    costDict[costType] = []
+
+for key in costDict.keys():
+    dfType = df[df["Type"] == key]
+    costTypePriceList = [float(price.replace("$", "")) for price in dfType['Amount']]
+    costDict[key] = "$%.2f"%sum(costTypePriceList)
+    
+print(costDict)
+
+
+
+
+
+
