837e2d87bfc11733c1ae608307cbe247a26c6782
reoyamanaka <ryamanaka807@gmail.com>
2021-01-15 17:51:47 -0500

    added graph feature

--- cost_visualizer.py
+++ cost_visualizer.py
@@ -13,9 +13,27 @@ for costType in df["Type"].unique():
 for key in costDict.keys():
     dfType = df[df["Type"] == key]
     costTypePriceList = [float(price.replace("$", "")) for price in dfType['Amount']]
-    costDict[key] = "$%.2f"%sum(costTypePriceList)
+    costDict[key] = sum(costTypePriceList)
     
-print(costDict)
+grandTotal = sum([value for value in costDict.values()])
+foodTotal = costDict['grocery'] + costDict['takeout'] + costDict['restaurant']
+
+costDict['food total'] = foodTotal
+costDict["total"] = grandTotal
+
+categories = [category for category in costDict.keys()]
+amounts = [amount for amount in costDict.values()]
+
+line = plt.bar(categories, amounts)
+plt.xticks(rotation=70)
+plt.xlabel('Category')
+plt.ylabel("Amount in CAD")
+plt.title("Cost Breakdown")
+for i in range(len(amounts)):
+    plt.annotate("%.2f"%amounts[i], xy=(categories[i],amounts[i]), ha='center', va='bottom')
+plt.gcf().subplots_adjust(bottom=0.30)
+plt.ylim(-1,1000)
+plt.show()
 
 
 
