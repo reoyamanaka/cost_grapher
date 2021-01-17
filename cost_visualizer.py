6578de02c7c20349a84fc8e0d8bd84f6e19ad960
reoyamanaka <ryamanaka807@gmail.com>
2021-01-16 21:10:11 -0500

    started analysis sheet

--- cost_visualizer.py
+++ cost_visualizer.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from datetime import date
 
 df = pd.read_csv('cost_sheet.csv', header = 0, index_col = 0)
 
@@ -37,7 +38,30 @@ plt.gcf().subplots_adjust(bottom=0.30)
 ylimMax = grandTotal + 200
 
 plt.ylim(-1,ylimMax)
+
+#creating and opening stats report
+unique_days = {df.index[0].split("-")[2]}
+
+for day in df.index:
+    unique_days.add(day.split('-')[2])    
+
+first_year, first_month, first_day = df.index[0].split("-")
+first_year, first_month, first_day = int(first_year), int(first_month), int(first_day)
+last_year, last_month, last_day = df.index[len(df)-1].split("-")
+last_year, last_month, last_day = int(last_year), int(last_month), int(last_day)
+
+first_date = date(first_year, first_month, first_day)
+last_date = date(last_year, last_month, last_day)
+delta = last_date - first_date
+num_of_days = delta.days + 1
+
+print("The average amount spent per day is $%.2f."%(grandTotal / num_of_days))
+print("The average amount spent per month is $%.2f."%(grandTotal / num_of_days * 30.436875))
+print("The average amount spent per year is $%.2f."%(grandTotal / num_of_days * 365.25))
+
 plt.show()
+
+
 
 
 
