22941524c0d926bad8f43f6f054b99848c849fee
reoyamanaka <ryamanaka807@gmail.com>
2021-01-16 23:46:47 -0500

    finished data file creation and opening functionality

--- cost_visualizer.py
+++ cost_visualizer.py
@@ -3,6 +3,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 from datetime import date
+import subprocess, os
 
 df = pd.read_csv('cost_sheet.csv', header = 0, index_col = 0)
 
@@ -32,14 +33,14 @@ plt.ylabel("Amount in CAD")
 plt.title("Cost Breakdown")
 for i in range(len(amounts)):
     plt.annotate("%.2f"%amounts[i], xy=(categories[i],amounts[i]), ha='center', va='bottom')
-plt.gcf().subplots_adjust(bottom=0.30)
+plt.gcf().subplots_adjust(bottom=0.35)
 
 #automatically adjust ylim max
 ylimMax = grandTotal + 200
 
 plt.ylim(-1,ylimMax)
 
-#creating and opening stats report
+#creating and opening data files
 unique_days = {df.index[0].split("-")[2]}
 
 for day in df.index:
@@ -55,11 +56,18 @@ last_date = date(last_year, last_month, last_day)
 delta = last_date - first_date
 num_of_days = delta.days + 1
 
-print("The average amount spent per day is $%.2f."%(grandTotal / num_of_days))
-print("The average amount spent per month is $%.2f."%(grandTotal / num_of_days * 30.436875))
-print("The average amount spent per year is $%.2f."%(grandTotal / num_of_days * 365.25))
+with open("cost_stats.txt", "w") as rf:
+    rf.write("The average amount spent per day is $%.2f.\n"%(grandTotal / num_of_days))
+    rf.write("The average amount spent per month is $%.2f.\n"%(grandTotal / num_of_days * 30.436875))
+    rf.write("The average amount spent per year is $%.2f."%(grandTotal / num_of_days * 365.25))
 
-plt.show()
+plt.savefig('cost_graph.png')
+subprocess.run(['open', "cost_stats.txt"], check=True)
+subprocess.run(['open', "cost_graph.png"], check=True)
+
+os._exit(0)
+
+
 
 
 
